b038fec14549c5d1975f1b22c0e30db8aeb80450
Joel Mosemann <mosemann@hotmail.com>
2017-12-05 20:43:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ playlist[artistName] = songTitle
 return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist.artistName
   return playlist
 }
